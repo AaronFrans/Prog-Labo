47d3f576528ff55c175eea5a47c75b9737251f4b
Aaron Frans <aaron.frans@student.hogent.be>
2020-02-17 14:23:29 +0100

    Update: Point

--- Labo Prog/Punt.cs
+++ Labo Prog/Punt.cs
@@ -6,6 +6,26 @@ namespace Labo_Prog
 {
     class Punt
     {
-        public
+        #region Functions
+        public override string ToString()
+        {
+            return $"X: {m_X}, Y: {m_Y}";
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is Punt punt &&
+                   m_X == punt.m_X &&
+                   m_Y == punt.m_Y;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(m_X, m_Y);
+        }
+        #endregion
+
+        #region Proprties
+        public float m_X { get; private set; }
+        public float m_Y { get; private set; }
+        #endregion
     }
 }
