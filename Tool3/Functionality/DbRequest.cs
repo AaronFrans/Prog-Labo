fc71617b80fdc21303be4e9a648fee16572e5962
Aaron Frans Thuis <aaron.frans@student.hogent.be>
2020-04-09 21:53:16 +0200

    Update: Query 2 and 3 working

--- Tool3/Functionality/DbRequest.cs
+++ Tool3/Functionality/DbRequest.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Objects;
+using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
 
 namespace Tool3
@@ -19,17 +21,27 @@ namespace Tool3
             return connection;
         }
 
+        #region stratenViaGemeenteNamen
         public void RequestStraatIDs(string gemeenteNaam)
         {
             List<int> straatIDs = RequestStraatIDsForGemeenteNaam(gemeenteNaam);
 
-            Console.WriteLine("*****************************");
-            Console.WriteLine("StraatIDs van gemeente {0}", gemeenteNaam);
-            foreach (var straatdID in straatIDs)
+            if (straatIDs != null)
             {
-                Console.WriteLine(straatdID);
+                Console.WriteLine("*****************************");
+                Console.WriteLine("StraatIDs van gemeente {0}", gemeenteNaam);
+                foreach (var straatdID in straatIDs)
+                {
+                    Console.WriteLine(straatdID);
+                }
+                Console.WriteLine("*****************************");
             }
-            Console.WriteLine("*****************************");
+            else
+            {
+                Console.WriteLine("*****************************");
+                Console.WriteLine("Geen straten gevonden voor gemeente {0}", gemeenteNaam);
+                Console.WriteLine("*****************************");
+            }
         }
         private List<int> RequestStraatIDsForGemeenteNaam(string gemeenteNaam)
         {
@@ -51,11 +63,19 @@ namespace Tool3
                 connection.Open();
 
                 SqlDataReader reader = command.ExecuteReader();
-                while(reader.Read())
+                if (reader.HasRows)
                 {
-                    int toAdd = (int)reader["StraatID"];
-                    toReturn.Add(toAdd);
+                    while (reader.Read())
+                    {
+                        int toAdd = (int)reader["StraatID"];
+                        toReturn.Add(toAdd);
+                    }
                 }
+                else
+                {
+                    toReturn = null;
+                }
+
 
                 reader.Close();
 
@@ -65,5 +85,381 @@ namespace Tool3
             return toReturn;
 
         }
+        #endregion
+
+        #region straatViaID
+        public void RequestStraat(int straatID)
+        {
+            Straat straatRequested = RequestStraatWithStraatID(straatID);
+            if (straatRequested != null)
+            {
+                Console.WriteLine("*****************************");
+                Console.WriteLine("Straat met id {0}", straatID);
+                straatRequested.ShowStraat();
+                Console.WriteLine("*****************************");
+            }
+            else
+            {
+                Console.WriteLine("*****************************");
+                Console.WriteLine("Straat met id {0} kan niet gemaakt worden", straatID);
+                Console.WriteLine("*****************************");
+            }
+
+        }
+        private Straat RequestStraatWithStraatID(int straatID)
+        {
+            Straat toReturn = null;
+            List<Segment> segmentensFromStraat = SegmentensFromStraat(straatID);
+
+            string query = "SELECT dbo.Straat.* " +
+                           "FROM dbo.Straat " +
+                           "SHERE dbo.Straat.ID = @straatID;";
+
+            using (SqlConnection connection = GetConnection())
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@straatID", straatID);
+
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    if (segmentensFromStraat != null)
+                    {
+                        while (reader.Read())
+                        {
+                            int graafID = (int)reader["GraafID"];
+                            Graaf toReturnGraaf = Graaf.BuildGraaf(graafID, segmentensFromStraat);
+                            int toReturnStraatID = (int)reader["ID"];
+                            string toReturnStraatNaam = (string)reader["Naam"];
+                            toReturn = new Straat(toReturnStraatID, toReturnStraatNaam, toReturnGraaf);
+                        }
+                    }
+                    else
+                    {
+                        toReturn = null;
+                    }
+                }
+                else
+                {
+                    toReturn = null;
+                }
+            }
+            return toReturn;
+        }
+        private Dictionary<int, List<Punt>> VerticesForEachSegment(int straatID)
+        {
+            Dictionary<int, List<Punt>> toReturn = new Dictionary<int, List<Punt>>();
+            string query = "SELECT dbo.Segment.ID as segmentID, dbo.SegmentVertices.PuntX, dbo.SegmentVertices.PuntY " +
+                           "FROM dbo.Straat " +
+                           "INNER JOIN dbo.Graaf " +
+                           "ON dbo.graaf.ID = dbo.straat.GraafID " +
+                           "INNER JOIN dbo.GraafMap " +
+                           "ON dbo.graaf.ID = dbo.GraafMap.GraafID " +
+                           "INNER JOIN dbo.Segment " +
+                           "ON dbo.Segment.ID = dbo.GraafMap.SegmentID " +
+                           "INNER JOIN dbo.SegmentVertices " +
+                           "ON dbo.SegmentVertices.SegmentID = dbo.Segment.ID " +
+                           "WHERE dbo.Straat.ID = @straatID;";
+
+
+
+            using (SqlConnection connection = GetConnection())
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@straatID", straatID);
+
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int segmentIDToAdd = (int)reader["segmentID"];
+                        Punt puntToAdd = new Punt((double)reader["PuntX"], (double)reader["PuntY"]);
+                        if (toReturn.ContainsKey(segmentIDToAdd))
+                        {
+                            toReturn[segmentIDToAdd].Add(puntToAdd);
+                        }
+                        else
+                            toReturn.Add(segmentIDToAdd, new List<Punt>() { puntToAdd });
+                    }
+                }
+                else
+                {
+                    toReturn = null;
+                }
+                reader.Close();
+
+                connection.Close();
+
+                return toReturn;
+            }
+        }
+        private List<Segment> SegmentensFromStraat(int straatID)
+        {
+            List<Segment> toReturn = new List<Segment>();
+            Dictionary<int, List<Punt>> verticesForEachSegment = VerticesForEachSegment(straatID);
+            string query = "Select dbo.Segment.ID as segmentID, dbo.Segment.BeginknoopID, dbo.Segment.EindknoopID " +
+                            "FROM dbo.Straat " +
+                            "INNER JOIN dbo.Graaf " +
+                            "ON dbo.graaf.ID = dbo.straat.GraafID " +
+                            "INNER JOIN dbo.GraafMap " +
+                            "ON dbo.graaf.ID = dbo.GraafMap.GraafID " +
+                            "INNER JOIN dbo.Segment " +
+                            "ON dbo.Segment.ID = dbo.GraafMap.SegmentID " +
+                            "WHERE dbo.Straat.ID = @straatID;";
+
+
+            using (SqlConnection connection = GetConnection())
+            {
+
+
+
+
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@straatID", straatID);
+
+
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    if (verticesForEachSegment != null)
+                    {
+                        while (reader.Read())
+                        {
+                            int segmentID = (int)reader["segmentID"];
+                            int beginKnoopID = (int)reader["BeginknoopID"];
+                            int eindKnoopId = (int)reader["EindknoopID"];
+
+                            Knoop beginKnoop = new Knoop(beginKnoopID, verticesForEachSegment[segmentID].First());
+                            Knoop eindKnoop = new Knoop(eindKnoopId, verticesForEachSegment[segmentID].Last());
+                            Segment toAdd = new Segment(segmentID, beginKnoop, eindKnoop, verticesForEachSegment[segmentID]);
+                            toReturn.Add(toAdd);
+
+                        }
+                    }
+                    else
+                    {
+                        toReturn = null;
+
+
+                    }
+                }
+                else
+                {
+                    toReturn = null;
+
+
+                }
+
+                reader.Close();
+
+                connection.Close();
+
+
+                return toReturn;
+            }
+        }
+        #endregion
+
+        #region StraatViaStraanEnGemeenteNaam
+        public void RequestStraat(string straatNaam, string gemeenteNaam)
+        {
+            Straat straatRequested = RequestStraatWithStraatNaamAndGemeenteNaam(straatNaam, gemeenteNaam);
+            if (straatRequested != null)
+            {
+                Console.WriteLine("*****************************");
+                Console.WriteLine("Straat met met naam {0} van gemeente met naam {1}", straatNaam, gemeenteNaam);
+                straatRequested.ShowStraat();
+                Console.WriteLine("*****************************");
+            }
+            else
+            {
+                Console.WriteLine("*****************************");
+                Console.WriteLine("Straat met met naam {0} van gemeente met naam {1} kan niet gemaakt worden", straatNaam , gemeenteNaam);
+                Console.WriteLine("*****************************");
+            }
+        }
+        private Straat RequestStraatWithStraatNaamAndGemeenteNaam(string straatNaam, string gemeenteNaam)
+        {
+            Straat toReturn = null;
+            List<Segment> segmentensFromStraat = SegmentensFromStraat(straatNaam, gemeenteNaam);
+
+            string query = "SELECT dbo.Straat.* " +
+                           "FROM dbo.Straat " +
+                           "INNER JOIN dbo.GemeenteStraaten " +
+                           "ON dbo.Straat.ID = dbo.GemeenteStraaten.StraatID " +
+                           "INNER JOIN dbo.Gemeente " +
+                           "ON dbo.Gemeente.ID = dbo.GemeenteStraaten.GemeenteID " +
+                           "WHERE dbo.Straat.Naam = @straatNaam " +
+                           "AND dbo.Gemeente.Naam = @gemeenteNaam;";
+
+            using (SqlConnection connection = GetConnection())
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@straatNaam", straatNaam);
+                command.Parameters.AddWithValue("@gemeenteNaam", gemeenteNaam);
+
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    if (segmentensFromStraat != null)
+                    {
+                        while (reader.Read())
+                        {
+                            int graafID = (int)reader["GraafID"];
+                            Graaf toReturnGraaf = Graaf.BuildGraaf(graafID, segmentensFromStraat);
+                            int toReturnStraatID = (int)reader["ID"];
+                            string toReturnStraatNaam = (string)reader["Naam"];
+                            toReturn = new Straat(toReturnStraatID, toReturnStraatNaam, toReturnGraaf);
+                        }
+                    }
+                    else
+                    {
+                        toReturn = null;
+                    }
+                }
+                else
+                {
+                    toReturn = null;
+                }
+            }
+            return toReturn;
+        }
+        private Dictionary<int, List<Punt>> VerticesForEachSegment(string straatNaam, string gemeenteNaam)
+        {
+            Dictionary<int, List<Punt>> toReturn = new Dictionary<int, List<Punt>>();
+            string query = "SELECT dbo.Segment.ID as segmentID, dbo.SegmentVertices.PuntX, dbo.SegmentVertices.PuntY " +
+                           "FROM dbo.Straat " +
+                           "INNER JOIN dbo.Graaf " +
+                           "ON dbo.graaf.ID = dbo.straat.GraafID " +
+                           "INNER JOIN dbo.GraafMap " +
+                           "ON dbo.graaf.ID = dbo.GraafMap.GraafID " +
+                           "INNER JOIN dbo.Segment " +
+                           "ON dbo.Segment.ID = dbo.GraafMap.SegmentID " +
+                           "INNER JOIN dbo.SegmentVertices " +
+                           "ON dbo.SegmentVertices.SegmentID = dbo.Segment.ID " +
+                           "INNER JOIN dbo.GemeenteStraaten " +
+                           "ON dbo.Straat.ID = dbo.GemeenteStraaten.StraatID " +
+                           "INNER JOIN dbo.Gemeente " +
+                           "ON dbo.Gemeente.ID = dbo.GemeenteStraaten.GemeenteID " +
+                           "WHERE dbo.Straat.Naam = @straatNaam " +
+                           "AND dbo.Gemeente.Naam = @gemeenteNaam;";
+
+
+
+            using (SqlConnection connection = GetConnection())
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@straatNaam", straatNaam);
+                command.Parameters.AddWithValue("@gemeenteNaam", gemeenteNaam);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int segmentIDToAdd = (int)reader["segmentID"];
+                        Punt puntToAdd = new Punt((double)reader["PuntX"], (double)reader["PuntY"]);
+                        if (toReturn.ContainsKey(segmentIDToAdd))
+                        {
+                            toReturn[segmentIDToAdd].Add(puntToAdd);
+                        }
+                        else
+                            toReturn.Add(segmentIDToAdd, new List<Punt>() { puntToAdd });
+                    }
+                }
+                else
+                {
+                    toReturn = null;
+                }
+                reader.Close();
+
+                connection.Close();
+
+                return toReturn;
+            }
+        }
+        List<Segment> SegmentensFromStraat(string straatNaam, string gemeenteNaam)
+        {
+            List<Segment> toReturn = new List<Segment>();
+            Dictionary<int, List<Punt>> verticesForEachSegment = VerticesForEachSegment(straatNaam, gemeenteNaam);
+            string query = "Select dbo.Segment.ID as segmentID, dbo.Segment.BeginknoopID, dbo.Segment.EindknoopID " +
+                           "FROM dbo.Straat " +
+                           "INNER JOIN dbo.Graaf " +
+                           "ON dbo.graaf.ID = dbo.straat.GraafID " +
+                           "INNER JOIN dbo.GraafMap " +
+                           "ON dbo.graaf.ID = dbo.GraafMap.GraafID " +
+                           "INNER JOIN dbo.Segment " +
+                           "ON dbo.Segment.ID = dbo.GraafMap.SegmentID " +
+                           "INNER JOIN dbo.GemeenteStraaten " +
+                           "ON dbo.Straat.ID = dbo.GemeenteStraaten.StraatID " +
+                           "INNER JOIN dbo.Gemeente " +
+                           "ON dbo.Gemeente.ID = dbo.GemeenteStraaten.GemeenteID " +
+                           "WHERE dbo.Straat.Naam = @straatNaam " +
+                           "AND dbo.Gemeente.Naam = @gemeenteNaam;";
+
+
+            using (SqlConnection connection = GetConnection())
+            {
+
+
+
+
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@straatNaam", straatNaam);
+                command.Parameters.AddWithValue("@gemeenteNaam", gemeenteNaam);
+
+
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    if (verticesForEachSegment != null)
+                    {
+                        while (reader.Read())
+                        {
+                            int segmentID = (int)reader["segmentID"];
+                            int beginKnoopID = (int)reader["BeginknoopID"];
+                            int eindKnoopId = (int)reader["EindknoopID"];
+
+                            Knoop beginKnoop = new Knoop(beginKnoopID, verticesForEachSegment[segmentID].First());
+                            Knoop eindKnoop = new Knoop(eindKnoopId, verticesForEachSegment[segmentID].Last());
+                            Segment toAdd = new Segment(segmentID, beginKnoop, eindKnoop, verticesForEachSegment[segmentID]);
+                            toReturn.Add(toAdd);
+
+                        }
+                    }
+                    else
+                    {
+                        toReturn = null;
+
+
+                    }
+                }
+                else
+                {
+                    toReturn = null;
+
+
+                }
+
+                reader.Close();
+
+                connection.Close();
+
+
+                return toReturn;
+            }
+        }
+        #endregion
+
     }
 }
